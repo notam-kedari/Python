7210c22ffe067f6ef6e9afa80a40b714577de827
NOTAM KEDARI <89922596+notam-kedari@users.noreply.github.com>
2024-07-06 07:20:56 +0530

    Positive Or Negative Integer

---
+++ Basic Programs/Positive_Or_Negative.py
@@ -0,0 +1,7 @@
+number=int(input())
+if number == 0 :
+  print("Zero")
+elif number>0 :
+  print("The given number",number," is a Positive integer")
+else:
+  print("The given number",number," is a Negative integer")
